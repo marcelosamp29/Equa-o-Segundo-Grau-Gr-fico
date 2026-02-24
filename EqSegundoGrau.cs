e449d961ac753a8bb273ef269e490fcfa6551e6d
Marcelo Sampaio <148807235+marcelosamp29@users.noreply.github.com>
2026-02-24 17:00:09 -0300

    Adicionar arquivo C#

---
+++ EqSegundoGrau.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjGrafico_Bhaskara
+{
+    public class EqSegundoGrau
+    {
+        private String a;
+        private String b;
+        private String c;
+
+        public void setA(String _a) { a = _a; }
+        public void setB(String _b) { b = _b; }
+        public void setC(String _c) { c = _c; }
+
+        public String getA() { return a; }
+        public String getB() { return b; }
+        public String getC() { return c; }
+        public String calcDelta()
+        {
+            float valorA = float.Parse(a);
+            float valorB = float.Parse(b);
+            float valorC = float.Parse(c);
+
+            return (Math.Pow(valorB, 2) - 4 * valorA * valorC).ToString();
+        }
+
+        public String calcX1(String delta)
+        {
+            float valorA = float.Parse(a);
+            float valorB = float.Parse(b);
+            //float valorC = float.Parse(c);
+            float valorDelta = float.Parse(delta);
+
+            return ((-valorB + Math.Sqrt(valorDelta)) / 2 * valorA).ToString("F3");
+        }
+
+        public String calcX2(String delta)
+        {
+            float valorA = float.Parse(a);
+            float valorB = float.Parse(b);
+            //float valorC = float.Parse(c);
+            float valorDelta = float.Parse(delta);
+
+            return ((-valorB - Math.Sqrt(valorDelta)) / 2 * valorA).ToString("F3");
+        }
+    }
+}
---
+++ Erro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjGrafico_Bhaskara
+{
+    public static class Erro
+    {
+        public static bool erro;
+        public static String mensagem;
+
+        public static void setErro(bool _erro) { erro = _erro; }
+        public static void setMensagem(String _mensagem) { erro = true; mensagem = _mensagem; }
+
+        public static String getMensagem() { return mensagem; }
+        public static bool getErro() { return erro; }
+    }
+}
